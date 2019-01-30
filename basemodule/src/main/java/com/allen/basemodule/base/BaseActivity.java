26e077f28c0e46443752b1ea551b234f25a62266
xjs <xujingshuai@quanxizhineng.com>
2019-01-30 10:30:03 +0800

    权限申请工具类及基类方法的封装

--- basemodule/src/main/java/com/allen/basemodule/base/BaseActivity.java
+++ basemodule/src/main/java/com/allen/basemodule/base/BaseActivity.java
@@ -2,6 +2,7 @@ package com.allen.basemodule.base;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,14 +10,20 @@ import android.view.View;
 import com.allen.basemodule.base.factory.PresenterFactory;
 import com.allen.basemodule.base.proxy.PresenterProxy;
 import com.allen.basemodule.config.Constants;
+import com.allen.basemodule.utils.PermissionUtil;
 import com.allen.basemodule.widget.stateview.StateView;
 import com.gyf.barlibrary.ImmersionBar;
+
+import java.util.List;
+
+import pub.devrel.easypermissions.AppSettingsDialog;
+import pub.devrel.easypermissions.EasyPermissions;
 
 /**
  * Created by xjs on 2019/1/17.
  */
 
-public abstract class BaseActivity<V extends BaseView, P extends BasePresenter<V>> extends AppCompatActivity implements BaseViewInterface {
+public abstract class BaseActivity<V extends BaseView, P extends BasePresenter<V>> extends AppCompatActivity implements BaseViewInterface, EasyPermissions.PermissionCallbacks {
     private PresenterProxy proxy = new PresenterProxy(PresenterFactory.<V, P>createFactory(getClass()));
     private StateView mStateView;
 
@@ -57,4 +64,46 @@ public abstract class BaseActivity<V extends BaseView, P extends BasePresenter<V
         outState.putBundle(Constants.PRESENTER_SAVE_KEY, proxy.onSaveInstanceState());
     }
 
+    /**
+     * 重写onRequestPermissionsResult，用于接受请求结果
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //将请求结果传递EasyPermission库处理
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    /**
+     * 请求权限成功。
+     * 可以弹窗显示结果，也可执行具体需要的逻辑操作
+     *
+     * @param requestCode
+     * @param perms
+     */
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+    }
+
+    /**
+     * 请求权限失败
+     *
+     * @param requestCode
+     * @param perms
+     */
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+        /**
+         * 若是在权限弹窗中，用户勾选了'NEVER ASK AGAIN.'或者'不在提示'，且拒绝权限。
+         * 这时候，需要跳转到设置界面去，让用户手动开启。
+         */
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            new AppSettingsDialog.Builder(this).build().show();
+        }
+    }
 }
--- basemodule/src/main/java/com/allen/basemodule/base/BaseFragment.java
+++ basemodule/src/main/java/com/allen/basemodule/base/BaseFragment.java
@@ -15,11 +15,16 @@ import com.allen.basemodule.config.Constants;
 import com.allen.basemodule.widget.stateview.StateView;
 import com.gyf.barlibrary.ImmersionBar;
 
+import java.util.List;
+
+import pub.devrel.easypermissions.AppSettingsDialog;
+import pub.devrel.easypermissions.EasyPermissions;
+
 /**
  * Created by xjs on 2019/1/17.
  */
 
-public abstract class BaseFragment<V extends BaseView, P extends BasePresenter<V>> extends Fragment implements BaseViewInterface {
+public abstract class BaseFragment<V extends BaseView, P extends BasePresenter<V>> extends Fragment implements BaseViewInterface, EasyPermissions.PermissionCallbacks  {
     private PresenterProxy proxy = new PresenterProxy(PresenterFactory.<V, P>createFactory(getClass()));
     private StateView mStateView;
     private View view;
@@ -61,4 +66,46 @@ public abstract class BaseFragment<V extends BaseView, P extends BasePresenter<V
         outState.putBundle(Constants.PRESENTER_SAVE_KEY, proxy.onSaveInstanceState());
     }
 
+    /**
+     * 重写onRequestPermissionsResult，用于接受请求结果
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //将请求结果传递EasyPermission库处理
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    /**
+     * 请求权限成功。
+     * 可以弹窗显示结果，也可执行具体需要的逻辑操作
+     *
+     * @param requestCode
+     * @param perms
+     */
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+    }
+
+    /**
+     * 请求权限失败
+     *
+     * @param requestCode
+     * @param perms
+     */
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+        /**
+         * 若是在权限弹窗中，用户勾选了'NEVER ASK AGAIN.'或者'不在提示'，且拒绝权限。
+         * 这时候，需要跳转到设置界面去，让用户手动开启。
+         */
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            new AppSettingsDialog.Builder(this).build().show();
+        }
+    }
 }
